11a238891ea02238affb385ddc0afa3c6618bb79
Your Name <snexmania76@gmail.com>
2025-10-29 03:38:05 +0800

    feat: adding type course
    Change-Id: Ia885991084668cce66cdfa4c721f88ad41771065

--- server/src/models/courses.ts
+++ server/src/models/courses.ts
@@ -1,5 +1,29 @@
-export type getCourseResponse = {
+export type Course = {
   id: number;
   nama_kursus: string;
   durasi: string;
+  gambar_url: string | null;
+  created_at: Date;
+  updated_at: Date;
 };
+
+export type CreateCourseRequest = {
+  nama_kursus: string;
+  durasi: string;
+  gambar_url?: string;
+};
+
+export type UpdateCourseRequest = {
+  nama_kursus?: string;
+  durasi?: string;
+  gambar_url?: string;
+};
+
+export type getCourseResponse = Course[];
+
+export type CreateCourseResponse = {
+  id: number;
+  nama_kursus: string;
+};
+
+export type UpdateCourseResponse = Course;
