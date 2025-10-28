d6100438aa86301d7723fa4290273b9588e04551
Your Name <snexmania76@gmail.com>
2025-10-29 03:40:31 +0800

    feat: using course and registration controller
    Change-Id: If07f2f18601b80032485f3aaaf5e5bfae161ba33

--- server/src/routes/public-api.ts
+++ server/src/routes/public-api.ts
@@ -1,5 +1,16 @@
 import express from "express";
-import { CoursesController } from "../controllers/courses_controller.js";
+import { CoursesController } from "../controllers/courses_controller";
+import { RegistrationController } from "../controllers/registration_controller";
 
 export const publicRouter = express.Router();
-publicRouter.get("/api/courses", CoursesController.get);
+
+// Course management routes
+publicRouter.get("/courses", CoursesController.get);
+publicRouter.post("/courses", CoursesController.create);
+publicRouter.put("/courses/:id", CoursesController.update);
+publicRouter.delete("/courses/:id", CoursesController.delete);
+
+// Registration routes
+publicRouter.post("/register", RegistrationController.register);
+publicRouter.get("/registrations", RegistrationController.getAll);
+publicRouter.delete("/registrations/:id", RegistrationController.delete);
