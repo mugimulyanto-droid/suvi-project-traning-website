35bb235f0b036cd80924499999ce4c8112670105
Your Name <snexmania76@gmail.com>
2025-10-29 03:37:21 +0800

    feat: modified course controller to use prisma query
    Change-Id: Id489888a65dafa49601d5be7a5f026ea8e1c22f4

--- server/src/controllers/courses_controller.ts
+++ server/src/controllers/courses_controller.ts
@@ -1,11 +1,53 @@
 import { type NextFunction, type Request, type Response } from "express";
-// import { type getCourseResponse } from "../models/courses";
+import { CoursesService } from "../services/courses_service";
+import {
+  type CreateCourseRequest,
+  type UpdateCourseRequest,
+} from "../models/courses";
 
 export class CoursesController {
   static async get(req: Request, res: Response, next: NextFunction) {
     try {
+      const courses = await CoursesService.getAll();
       res.status(200).json({
-        data: "oke",
+        data: courses,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      const request: CreateCourseRequest = req.body;
+      const course = await CoursesService.create(request);
+      res.status(201).json({
+        data: course,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async update(req: Request, res: Response, next: NextFunction) {
+    try {
+      const id = parseInt(req.params.id);
+      const request: UpdateCourseRequest = req.body;
+      const course = await CoursesService.update(id, request);
+      res.status(200).json({
+        data: course,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async delete(req: Request, res: Response, next: NextFunction) {
+    try {
+      const id = parseInt(req.params.id);
+      const message = await CoursesService.delete(id);
+      res.status(200).json({
+        message: message,
       });
     } catch (error) {
       next(error);
